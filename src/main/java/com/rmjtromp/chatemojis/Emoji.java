dfa421a9b6eaa73fecce9fc5244d11ebc0fb53a1
iStudLion <istudlions@gmail.com>
2021-03-06 10:38:55 -0400

    Documented Methods & Added Multiple Language Support

--- src/main/java/com/rmjtromp/chatemojis/Emoji.java
+++ src/main/java/com/rmjtromp/chatemojis/Emoji.java
@@ -1,11 +1,11 @@
 package com.rmjtromp.chatemojis;
 
 import static com.rmjtromp.chatemojis.ChatEmojis.NAME_PATTERN;
+import static com.rmjtromp.chatemojis.ChatEmojis.RANDOM;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,22 +16,27 @@ import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
+import com.rmjtromp.chatemojis.ChatEmojis.AbstractEmoji;
 import com.rmjtromp.chatemojis.exceptions.ConfigException;
 import com.rmjtromp.chatemojis.exceptions.InvalidEmojiException;
 import com.rmjtromp.chatemojis.exceptions.InvalidEmoticonException;
 import com.rmjtromp.chatemojis.exceptions.InvalidRegexException;
-import com.rmjtromp.chatemojis.utils.ComponentUtils;
+import com.rmjtromp.chatemojis.utils.Lang;
+import com.rmjtromp.chatemojis.utils.Lang.Replacements;
+import com.rmjtromp.chatemojis.utils.bukkit.ComponentUtils;
+import com.rmjtromp.chatemojis.utils.bukkit.Version;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
 
-class Emoji {
+class Emoji implements AbstractEmoji {
 
     private static final Pattern REGEX_PATTERN = Pattern.compile("^/(.+)/(i)?$", Pattern.CASE_INSENSITIVE);
-    private static final ChatEmojis plugin = ChatEmojis.getInstance();
+    private static final ChatEmojis PLUGIN = ChatEmojis.getInstance();
 
     private final String name;
     private final EmojiGroup parent;
@@ -45,54 +50,71 @@ class Emoji {
         return new Emoji(Objects.requireNonNull(parent), Objects.requireNonNull(section));
     }
 
+    /**
+     * Creates an emoji from the {@link ConfigurationSection} provided,
+     * and links the {@link Permission} node to the specified {@link EmojiGroup}
+     * @param parent
+     * @param section
+     * @throws ConfigException
+     */
     private Emoji(EmojiGroup parent, ConfigurationSection section) throws ConfigException {
         this.parent = parent;
         Matcher matcher = NAME_PATTERN.matcher(section.getCurrentPath());
         if(matcher.find()) {
             name = matcher.group(1).replaceAll("[_\\s]", "-").replaceAll("[^0-9a-zA-Z-]", "");
-            if(name.isEmpty()) throw new ConfigException("Emoji name can not be empty", section);
-        } else throw new ConfigException("Invalid emoji name", section);
+            if(name.isEmpty()) throw new ConfigException(Lang.translate("error.emoji.name.empty"), section);
+        } else throw new ConfigException(Lang.translate("error.emoji.name.invalid"), section);
+        
+        /*
+         * Set the permission node, inherit name of parent groups.
+         */
         permission = parent.parentNames.isEmpty() ? new Permission(String.format("chatemojis.use.%s", name.toLowerCase())) : new Permission(String.format("chatemojis.use.%s.%s", String.join(".", parent.parentNames).toLowerCase(), name.toLowerCase()));
         permission.setDefault(PermissionDefault.OP);
         permission.setDescription(String.format("Permission to use '%s' emoji", name));
         permission.addParent(parent.getPermission(), true);
         
+        /*
+         * Loops through all keys and checks for a key that matches /emoticons?/i
+         * This allows for typos to be made
+         */
         boolean emoticonFound = false;
         for(String key : section.getKeys(false)) {
             if(key.toLowerCase().matches("^emoticons?$")) {
                 if(section.isString(key) || section.isInt(key)) {
                     String emoticon = section.isString(key) ? section.getString(key) : Integer.toString(section.getInt(key));
                     if(!emoticon.isEmpty()) emoticons.add(emoticon);
-                    else throw new InvalidEmoticonException("Emoticon can not be empty", section);
+                    else throw new InvalidEmoticonException(Lang.translate("error.emoji.emoticon.empty"), section);
                 } else if(section.isList(key)) {
                     for(Object obj : section.getList(key)) {
                         String emoticon;
                         if(obj instanceof String) emoticon = (String) obj;
                         else if(obj instanceof Integer) emoticon = Integer.toString((Integer) obj);
                         else if(obj instanceof Boolean) emoticon = Boolean.toString((Boolean) obj);
-                        else throw new InvalidEmoticonException("Emoticon type not supported", section);
+                        else throw new InvalidEmoticonException(Lang.translate("error.emoji.emoticon.not-supported"), section);
 
                         if(!emoticon.isEmpty()) emoticons.add(emoticon);
-                        else throw new InvalidEmoticonException("Emoticon can not be empty", section);
+                        else throw new InvalidEmoticonException(Lang.translate("error.emoji.emoticon.empty"), section);
                     }
                 }
                 emoticonFound = true;
                 break;
             }
         }
-        if(emoticons.isEmpty()) throw new InvalidEmoticonException("Emoticon type not supported", section);
-        else if(!emoticonFound) throw new InvalidEmoticonException("No emoticon provided", section);
+        if(emoticons.isEmpty()) throw new InvalidEmoticonException(Lang.translate("error.emoji.emoticon.not-supported"), section);
+        else if(!emoticonFound) throw new InvalidEmoticonException(Lang.translate("error.emoji.emoticon.not-provided"), section);
 
+        /*
+         * Loops through all keys and checks for a key that matches /regex/i
+         * This allows for typos to be made
+         */
         Pattern pattern = null;
         for(String key : section.getKeys(false)) {
             if(key.equalsIgnoreCase("regex")) {
                 if(section.isString(key)) {
                     String value = section.getString(key);
                     Matcher m = REGEX_PATTERN.matcher(value);
-                    if(m.matches()) {
-                        pattern = m.group(2) != null ? Pattern.compile(m.group(1), Pattern.CASE_INSENSITIVE) : Pattern.compile(m.group(1));
-                        break;
-                    } else throw new InvalidRegexException("Invalid regex syntax", section);
+                    if(m.matches()) pattern = m.group(2) != null ? Pattern.compile(m.group(1), Pattern.CASE_INSENSITIVE) : Pattern.compile(m.group(1));
+                    else throw new InvalidRegexException(Lang.translate("error.emoji.regex.invalid"), section);
                 }
                 break;
             }
@@ -101,32 +123,39 @@ class Emoji {
         emoticons.forEach(emoticon -> quotedEmoticons.add(Pattern.quote(emoticon)));
         this.pattern = pattern != null ? pattern : Pattern.compile(String.format("(%s)", String.join("|", quotedEmoticons)), Pattern.CASE_INSENSITIVE);
 
+        /*
+         * Loops through all keys and checks for a key that matches /emojis?/i
+         * This allows for typos to be made
+         */
         boolean emojiFound = false;
         for(String key : section.getKeys(false)) {
             if(key.toLowerCase().matches("^emojis?$")) {
                 if(section.isString(key) || section.isInt(key)) {
                     String emoji = section.isString(key) ? section.getString(key) : Integer.toString(section.getInt(key));
                     if(!emoji.isEmpty()) emojis.add(ChatColor.translateAlternateColorCodes('&', StringEscapeUtils.unescapeJava(emoji)));
-                    else throw new InvalidEmojiException("Emoji can not be empty", section);
+                    else throw new InvalidEmojiException(Lang.translate("error.emoji.emoji.empty"), section);
                 } else if(section.isList(key)) {
                     for(Object obj : section.getList(key)) {
                         String emoji;
                         if(obj instanceof String) emoji = (String) obj;
                         else if(obj instanceof Integer) emoji = Integer.toString((Integer) obj);
                         else if(obj instanceof Boolean) emoji =  Boolean.toString((Boolean) obj);
-                        else throw new InvalidEmojiException("Emoji type not supported", section);
+                        else throw new InvalidEmojiException(Lang.translate("error.emoji.emoji.not-supported"), section);
 
                         if(!emoji.isEmpty()) emojis.add(ChatColor.translateAlternateColorCodes('&', StringEscapeUtils.unescapeJava(emoji)));
-                        else throw new InvalidEmojiException("Emoji can not be empty", section);
+                        else throw new InvalidEmojiException(Lang.translate("error.emoji.emoji.empty"), section);
                     }
                 }
                 emojiFound = true;
                 break;
             }
         }
-        if(emojis.isEmpty()) throw new InvalidEmojiException("Emoji type not supported", section);
-        else if(!emojiFound) throw new InvalidEmojiException("No emoji provided", section);
+        if(emojis.isEmpty()) throw new InvalidEmojiException(Lang.translate("error.emoji.emoji.not-supported"), section);
+        else if(!emojiFound) throw new InvalidEmojiException(Lang.translate("error.emoji.emoji.not-provided"), section);
 
+        /*
+         * Checks whether or not the emoji should be disabled
+         */
         for(String key : section.getKeys(false)) {
             if(key.equalsIgnoreCase("enabled")) {
                 Object obj = section.get(key);
@@ -144,70 +173,123 @@ class Emoji {
         }
     }
 
+    /**
+     * Returns the name of the {@link Emoji}
+     */
+    @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns emoticons of the {@link Emoji}
+     * (e.x. :music:)
+     */
     public List<String> getEmoticons() {
         return new ArrayList<>(emoticons);
     }
 
+    /**
+     * Returns all possible emojis outcome of the {@link Emoji}
+     */
     public List<String> getEmojis() {
         return new ArrayList<>(emojis);
     }
 
-    private static final Random random = new Random();
+    /**
+     * Returns a random emoji if there are multiple
+     * returns the first one if there's only one
+     */
     public String getEmoji() {
-        return getEmojis().size() > 1 ? getEmojis().get(random.nextInt(getEmojis().size())) : getEmojis().get(0);
+        return getEmojis().size() > 1 ? getEmojis().get(RANDOM.nextInt(getEmojis().size())) : getEmojis().get(0);
     }
 
+    /**
+     * Returns the {@link Pattern} that is used to find
+     * emoticon matches
+     */
     public Pattern getPattern() {
         return pattern;
     }
 
+    /**
+     * Returns whether or not an emoji is enabled
+     */
     public boolean isEnabled() {
         return enabled;
     }
     
+    /**
+     * Enabled the emoji
+     */
     public void enable() {
     	enabled = true;
     }
     
+    /**
+     * Disables the emoji
+     */
     public void disable() {
     	enabled = false;
     }
 
+    /**
+     * Returns the {@link Permission} node for the {@link Emoji}
+     */
+    @Override
     public Permission getPermission() {
         return permission;
     }
 
+    /**
+     * Returns the {@link EmojiGroup} for the {@link Emoji}
+     */
     public EmojiGroup getParent() {
         return parent;
     }
 
+    /**
+     * Parses a string
+     * replaces all emoticons with emojis
+     * @param player
+     * @param resetColor
+     * @param message
+     */
     String parse(Player player, String resetColor, String message) {
         if(isEnabled() && player.hasPermission(getPermission())) {
             Matcher matcher = getPattern().matcher(message);
             while(matcher.find()) {
             	if(getEmojis().size() > 1) {
-            		message = matcher.replaceFirst(ChatColor.RESET + (!plugin.isPapiLoaded() ? getEmoji() : PlaceholderAPI.setPlaceholders(player, getEmoji())) + resetColor);
+            		message = matcher.replaceFirst(ChatColor.RESET + (!PLUGIN.papiIsLoaded ? getEmoji() : PlaceholderAPI.setPlaceholders(player, getEmoji())) + resetColor);
             		matcher = getPattern().matcher(message);
-            	} else message = matcher.replaceAll(ChatColor.RESET + (!plugin.isPapiLoaded() ? getEmoji() : PlaceholderAPI.setPlaceholders(player, getEmoji())) + resetColor);
+            	} else message = matcher.replaceAll(ChatColor.RESET + (!PLUGIN.papiIsLoaded ? getEmoji() : PlaceholderAPI.setPlaceholders(player, getEmoji())) + resetColor);
             }
         }
         return message;
     }
 
+    /**
+     * Returns {@link TextComponent} array to display in
+     * list for each emoticons.
+     * @param player
+     */
+	@SuppressWarnings("deprecation")
 	List<TextComponent> getComponent(Player player) {
         List<TextComponent> components = new ArrayList<>();
-        TextComponent prefix = ComponentUtils.createComponent("&7  - ");
         emoticons.forEach(emoticon -> {
-            TextComponent comp = ComponentUtils.createComponent(String.format("&e%s &7- &f%s", emoticon, getPattern().matcher(emoticon).replaceAll(PlaceholderAPI.setPlaceholders(player, getEmoji()))));
-            if(player.isOp()) {
-                comp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(ComponentUtils.createBaseComponent("&fPermission Node:\n&7"+getPermission().getName()+"\n\n&eClick To Copy!"))));
-                comp.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, getPermission().toString()));
+            TextComponent comp = ComponentUtils.createComponent(String.format("&7  - &e%s &7- &f%s", emoticon, parse(player, ChatColor.RESET+"", emoticon)));
+            if(player.hasPermission("chatemojis.admin")) {
+            	BaseComponent[] hoverMessage = ComponentUtils.createBaseComponent(Lang.translate("error.emoji.hover-component", Replacements.singleton("permission", permission.getName())));
+            	
+            	// new Text(BaseComponent[]) is not added until 1.16
+            	if(Version.getServerVersion().isOlderThan(Version.V1_16)) comp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverMessage));
+            	else comp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(hoverMessage)));
+            	
+            	// ClickEvent.Action.COPY_TO_CLIPBOARD is not added until 1.15
+            	if(Version.getServerVersion().isOlderThan(Version.V1_15)) comp.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, getPermission().getName()));
+            	else comp.setClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, getPermission().getName()));
             }
-            components.add(ComponentUtils.mergeComponents(prefix, comp));
+            components.add(comp);
         });
         return components;
     }
--- src/main/java/com/rmjtromp/chatemojis/EmojiGroup.java
+++ src/main/java/com/rmjtromp/chatemojis/EmojiGroup.java
@@ -1,24 +1,31 @@
 package com.rmjtromp.chatemojis;
 
-import com.rmjtromp.chatemojis.ChatEmojis.Emojis;
-import com.rmjtromp.chatemojis.exceptions.ConfigException;
-import com.rmjtromp.chatemojis.ChatEmojis.EmojiGroups;
-import net.md_5.bungee.api.chat.TextComponent;
+import static com.rmjtromp.chatemojis.ChatEmojis.NAME_PATTERN;
+import static com.rmjtromp.chatemojis.ChatEmojis.RESERVED_NAMES;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
-import java.util.*;
-import java.util.regex.Matcher;
+import com.rmjtromp.chatemojis.ChatEmojis.AbstractEmoji;
+import com.rmjtromp.chatemojis.exceptions.ConfigException;
+import com.rmjtromp.chatemojis.utils.Lang;
+import com.rmjtromp.chatemojis.utils.Lang.Replacements;
 
-import static com.rmjtromp.chatemojis.ChatEmojis.NAME_PATTERN;
-import static com.rmjtromp.chatemojis.ChatEmojis.RESERVED_NAMES;
+import net.md_5.bungee.api.chat.TextComponent;
 
-class EmojiGroup {
-
-    private final Emojis emojis = new Emojis();
-    private final EmojiGroups groups = new EmojiGroups();
+class EmojiGroup implements AbstractEmoji {
+	
+    private final List<Emoji> emojis = new ArrayList<>();
+    private final List<EmojiGroup> groups = new ArrayList<>();
 
     private final String name;
     private final Permission permission;
@@ -30,16 +37,24 @@ class EmojiGroup {
         return new EmojiGroup(null, Objects.requireNonNull(section));
     }
 
+    /**
+     * Creates an {@link EmojiGroup} from the {@link ConfigurationSection} provided
+     * @param parent
+     * @param section
+     * @throws ConfigException
+     */
     private EmojiGroup(EmojiGroup parent, ConfigurationSection section) throws ConfigException {
         this.parent = parent;
-        String _permission = "chatemojis.use";
+        String permissionBase = "chatemojis.use";
         if(parent != null) {
+        	// scrape name from the ConfigurationSection path
             Matcher matcher = NAME_PATTERN.matcher(section.getCurrentPath());
             if(matcher.find()) {
                 name = matcher.group(1).replaceAll("[_\\s]+", "-").replaceAll("[^0-9a-zA-Z-]", "");
-                if(name.isEmpty()) throw new ConfigException("Group name can not be empty", section);
-            } else throw new ConfigException("Invalid group name", section);
+                if(name.isEmpty()) throw new ConfigException(Lang.translate("error.emojigroup.name.empty"), section);
+            } else throw new ConfigException(Lang.translate("error.emojigroup.name.invalid"), section);
 
+            // build the permission node base
             parentNames.add(this.name);
             EmojiGroup emoji = this;
             while(emoji.parent != null && emoji.parent.name != null) {
@@ -48,44 +63,85 @@ class EmojiGroup {
             }
             Collections.reverse(parentNames);
 
-            _permission = String.format("%s.%s", _permission, String.join(".", parentNames).toLowerCase());
+            permissionBase = String.format("%s.%s", permissionBase, String.join(".", parentNames).toLowerCase());
         } else name = null;
-        permission = new Permission(String.format("%s.*", _permission));
+        
+        /*
+         * Set the permission node, inherit name of parent groups.
+         */
+        permission = new Permission(String.format("%s.*", permissionBase));
         permission.setDefault(PermissionDefault.OP);
         permission.setDescription(String.format("Permission to use all emojis listed in '%s'", name));
         if(parent != null) permission.addParent(parent.getPermission(), true);
         
+        /*
+         * Loops through all keys and looks for anything that is not reserved
+         * in the case of the first group (default group) "settings" is also reserved
+         */
         for(String key : section.getKeys(false)) {
-            if(!RESERVED_NAMES.contains(key.toLowerCase()) || (parent == null && !key.equalsIgnoreCase("settings"))) {
+            if(!RESERVED_NAMES.contains(key.toLowerCase())) {
                 ConfigurationSection s = section.getConfigurationSection(key);
                 if(s == null) continue;
                 if(isSetKey(s, "^emoticons?$") && isSetKey(s, "^emojis?$")) {
                     try {
                         emojis.add(Emoji.init(this, s));
                     } catch (ConfigException e) {
-                        System.out.println("[ChatEmoji] There was an error loading emoji: "+e.getMessage());
+                    	Replacements replacements = new Replacements();
+                    	replacements.add("emoji", key);
+                    	replacements.add("message", e.getMessage());
+                        System.out.println(String.format("[ChatEmoji] %s", Lang.translate("error.load.emoji", replacements)));
                     }
-                } else groups.add(new EmojiGroup(this, s));
+                } else {
+                    try {
+                    	groups.add(new EmojiGroup(this, s));
+                    } catch (ConfigException e) {
+                    	Replacements replacements = new Replacements();
+                    	replacements.add("emojigroup", key);
+                    	replacements.add("message", e.getMessage());
+                        System.out.println(String.format("[ChatEmoji] %s", Lang.translate("error.load.group", replacements)));
+                    }
+                }
             }
         }
     }
 
+    /**
+     * Returns the name of the {@link EmojiGroup}
+     */
+    @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the {@link Permission} node of the {@link EmojiGroup}
+     */
+    @Override
     public Permission getPermission() {
         return permission;
     }
 
-    public EmojiGroups getGroups() {
+    /**
+     * Returns all child {@link EmojiGroup}s
+     */
+    public List<EmojiGroup> getGroups() {
         return groups;
     }
 
-    public Emojis getEmojis() {
+    /**
+     * Returms all child {@link Emoji}
+     */
+    public List<Emoji> getEmojis() {
         return emojis;
     }
-
+    
+    /**
+     * Parses a string, replaces all emoticons with emojis.
+     * Does this for all sub-groups and child emojis
+     * @param player
+     * @param resetColor
+     * @param message
+     */
     String parse(Player player, String resetColor, String message) {
         for(EmojiGroup group : getGroups()) message = group.parse(player, resetColor, message);
         for(Emoji emoji : getEmojis()) message = emoji.parse(player, resetColor, message);
@@ -93,6 +149,11 @@ class EmojiGroup {
 
     }
 
+    /**
+     * Returns {@link TextComponent} array to display in
+     * list for each emoticons.
+     * @param player
+     */
     List<TextComponent> getComponents(Player player) {
         List<TextComponent> components = new ArrayList<>();
         getGroups().forEach(group -> components.addAll(group.getComponents(player)));
@@ -100,11 +161,21 @@ class EmojiGroup {
         return components;
     }
 
+    /**
+     * Checks if key exists in {@link ConfigurationSecion} that matches the regular expression
+     * @param section
+     * @param regex
+     */
     private boolean isSetKey(ConfigurationSection section, String regex) {
         for(String key : section.getKeys(false)) {
             if(key.toLowerCase().matches(regex)) return true;
         }
         return false;
     }
+
+	public void forEach(Consumer<AbstractEmoji> action) {
+		groups.forEach(action);
+		emojis.forEach(action);
+	}
 
 }
