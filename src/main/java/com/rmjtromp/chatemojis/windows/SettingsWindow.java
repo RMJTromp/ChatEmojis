08f905253576935caf29d449a95df7bbf812d37b
Raygell <me@rmjtromp.com>
2022-07-21 22:34:41 +0200

    Added play Note Block Pling sound when interacting with settings

--- src/main/java/com/rmjtromp/chatemojis/windows/SettingsWindow.java
+++ src/main/java/com/rmjtromp/chatemojis/windows/SettingsWindow.java
@@ -5,12 +5,13 @@ import com.rmjtromp.chatemojis.utils.Version;
 import com.rmjtromp.chatemojis.utils.Window;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,6 +22,9 @@ public class SettingsWindow extends Window {
     private final ChatEmojis PLUGIN;
 
     private final ItemStack BOOK, SIGN, CLOSE;
+
+    @Nullable
+    private final Sound PLING_SOUND;
 
     public SettingsWindow(@NotNull ChatEmojis plugin) {
         super(null, InventoryType.HOPPER, "ChatEmojis Settings");
@@ -43,7 +47,6 @@ public class SettingsWindow extends Window {
         List<String> bookLore = new ArrayList<>();
         Arrays.asList("&7Click this item to toggle whether or not", "&7emojis can be used in books.").forEach(lore -> bookLore.add(ChatColor.translateAlternateColorCodes('&', lore)));
         bookMeta.setLore(bookLore);
-        if(Version.getServerVersion().isNewerThan(Version.V1_7)) bookMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         BOOK.setItemMeta(bookMeta);
 
         SIGN = new ItemStack(Version.getServerVersion().isNewerThan(Version.V1_13) ? Material.valueOf("OAK_SIGN") : Material.valueOf("SIGN"));
@@ -53,8 +56,16 @@ public class SettingsWindow extends Window {
         List<String> signLore = new ArrayList<>();
         Arrays.asList("&7Click this item to toggle whether or not", "&7emojis can be used on signs.").forEach(lore -> signLore.add(ChatColor.translateAlternateColorCodes('&', lore)));
         signMeta.setLore(signLore);
-        if(Version.getServerVersion().isNewerThan(Version.V1_7)) signMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         SIGN.setItemMeta(signMeta);
+
+        Sound plingSound = null;
+        for(String s : new String[]{"NOTE_PLING", "BLOCK_NOTE_PLING", "BLOCK_NOTE_BLOCK_PLING"}) {
+            try {
+                plingSound = Sound.valueOf(s);
+                break;
+            } catch(Exception ignore) {}
+        }
+        PLING_SOUND = plingSound;
     }
 
     @Override
@@ -72,27 +83,44 @@ public class SettingsWindow extends Window {
 
         setItemEvents(4, close).onClick(e -> e.getWhoClicked().closeInventory());
 
-        if(useInBooks) {
+        {
             ItemMeta bookMeta = book.getItemMeta();
             assert bookMeta != null;
-            bookMeta.addEnchant(Enchantment.LURE, 1, true);
-            book.setItemMeta(bookMeta);
-        }
-        setItemEvents(0, book).onClick(e -> {
-            PLUGIN.useInBooks.setValue(!useInBooks);
-            build();
-        });
 
-        if(useOnSigns) {
+            List<String> lores = bookMeta.hasLore() ? bookMeta.getLore() : new ArrayList<>();
+            assert lores != null;
+            lores.add("§7");
+            lores.add(useInBooks ? "§a§lENABLED" : "§c§lDISABLED");
+            bookMeta.setLore(lores);
+
+            book.setItemMeta(bookMeta);
+
+            setItemEvents(0, book).onClick(e -> {
+                PLUGIN.useInBooks.setValue(!useInBooks);
+                if(PLING_SOUND != null) ((Player) e.getWhoClicked()).playSound(e.getWhoClicked().getLocation(), PLING_SOUND, 1f, 1f);
+                build();
+            });
+        }
+
+        {
             ItemMeta signMeta = sign.getItemMeta();
             assert signMeta != null;
-            signMeta.addEnchant(Enchantment.LURE, 1, true);
-            book.setItemMeta(signMeta);
+
+            List<String> lores = signMeta.hasLore() ? signMeta.getLore() : new ArrayList<>();
+            assert lores != null;
+            lores.add("§7");
+            lores.add(useOnSigns ? "§a§lENABLED" : "§c§lDISABLED");
+            signMeta.setLore(lores);
+
+            sign.setItemMeta(signMeta);
+
+            setItemEvents(1, sign).onClick(e -> {
+                PLUGIN.useOnSigns.setValue(!useOnSigns);
+                if(PLING_SOUND != null) ((Player) e.getWhoClicked()).playSound(e.getWhoClicked().getLocation(), PLING_SOUND, 1f, 1f);
+                build();
+            });
         }
-        setItemEvents(1, sign).onClick(e -> {
-            PLUGIN.useOnSigns.setValue(!useOnSigns);
-            build();
-        });
+
     }
 
 }
